9d9b3c61ce41c2e145526f1e9cfa06472dbe9525
baptistepouget <babadu83510@gmail.com>
2016-01-16 20:59:50 +0100

    Added some obstacles !

--- CRAZYCAR/CRAZYCAR.ino
+++ CRAZYCAR/CRAZYCAR.ino
@@ -3,16 +3,20 @@
 #include <Buttons.h>
 #include <Display.h>
 #include <Sound.h>
+#include "Struct.h"
 //imports the SPI library (needed to communicate with Gamebuino's screen)
 #include <SPI.h>
 //imports the Gamebuino library
 #include <Gamebuino.h>
+#define LIMITEROUTEH 36
+#define LIMITEROUTEB 12
+#define LIMITEROUTEM 24
+#define NOMBREPOINTILLES 4
 //creates a Gamebuino object named gb
 Gamebuino gb;
 
 int voitureX=8, voitureY=16, i;
-const int limiteRouteH=36, limiteRouteB=12,limiteRouteM=24,nombrePointilles=4;
-int  espacePointilles = 4,taillePointille=LCDWIDTH/nombrePointilles-espacePointilles;
+int  espacePointilles = 4,taillePointille=LCDWIDTH/NOMBREPOINTILLES-espacePointilles;
 const byte voiture[] PROGMEM = {16,8,
 B00000111,B11110000,
 B00001011,B11010000,
@@ -23,26 +27,67 @@ B11111111,B11111111,
 B11001110,B00100111,
 B01111000,B00111100,
 };
-const byte animal[] PROGMEM = {8,8,0x5A,0x5B,0x7F,0xFE,0x7E,0x7C,0x74,0x6C,};
-const byte clous[] PROGMEM = {8,8,0x5A,0x5A,0x7E,0x1C,0x1E,0x3C,0x74,0x6C,};
-const byte cone[] PROGMEM = {8,8,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,};
-const byte homme[] PROGMEM = {8,8,0x5A,0x5A,0x7E,0x18,0x18,0x3C,0x24,0x6C,};
-
+const byte animal[] PROGMEM = {8,8,
+B00000010,
+B00000011,
+B01111111,
+B11111110,
+B01111110,
+B01000100,
+B01000100,
+B00000000,
+};
+const byte clous[] PROGMEM = {8,8,
+B00000000,
+B00010000,
+B00111000,
+B00000100,
+B00001110,
+B00100000,
+B01110000,
+B00000000,
+};
+const byte cone[] PROGMEM = {8,8,
+B00000000,
+B00011000,
+B00111100,
+B00100100,
+B01111110,
+B01000010,
+B11111111,
+B11111111,
+};
+const byte homme[] PROGMEM = {8,8,
+B01011010,
+B01011010,
+B01111110,
+B00011000,
+B00011000,
+B00111100,
+B00100100,
+B01101100,
+};
 
 void dVoiture();
 void dDecor();
 void dObstacles();
+
+typedef struct Obstacle Obstacle;
 struct Obstacle
 {
   int x = 0;
   int y = 0;
-  int nbitmap = 0;
+  int nBitmap = 0;
 };
-struct Obstacle obA, obB, obC;
-obA.x =
+Obstacle obs[3];
+
 // the setup routine runs once when Gamebuino starts up
 void setup(){
   // initialize the Gamebuino object
+  obs[0].x = -8;
+  obs[1].x = 30;
+  obs[2].x = 60;
+  obs[0].y = obs[1].y = obs[2].y = 12;
   gb.begin();
   // shows the start menu
   gb.titleScreen(F("Crazycar"));
@@ -56,18 +101,18 @@ void loop(){
   if(gb.update()){
     dDecor();
     dVoiture();
-    gb.display.drawBitmap(voitureX,voitureY,cone);
+    dObstacles();
   }
 }
 
 void dVoiture()
 {
-  
-    if ((gb.buttons.repeat(BTN_DOWN,1))&&((voitureY+8)<limiteRouteH))
+
+    if ((gb.buttons.repeat(BTN_DOWN,1))&&((voitureY+8)<LIMITEROUTEH))
     {
       voitureY+=2;
     }
-    if ((gb.buttons.repeat(BTN_UP,1))&&(voitureY>limiteRouteB))
+    if ((gb.buttons.repeat(BTN_UP,1))&&(voitureY>LIMITEROUTEB))
     {
       voitureY-=2;
     }
@@ -76,18 +121,38 @@ void dVoiture()
 }
 void dDecor()
 {
-      gb.display.drawLine(0,limiteRouteH,LCDWIDTH,limiteRouteH);
-    gb.display.drawLine(0,limiteRouteB,LCDWIDTH,limiteRouteB);
-    for (i=0;i<nombrePointilles+1;i++)
+      gb.display.drawLine(0,LIMITEROUTEH,LCDWIDTH,LIMITEROUTEH);
+    gb.display.drawLine(0,LIMITEROUTEB,LCDWIDTH,LIMITEROUTEB);
+    for (i=0;i<NOMBREPOINTILLES+1;i++)
     {
-      gb.display.drawLine(espacePointilles+i*(taillePointille+8),limiteRouteM,espacePointilles+i*(taillePointille+8)+taillePointille,limiteRouteM); 
+      gb.display.drawLine(espacePointilles+i*(taillePointille+8),LIMITEROUTEM,espacePointilles+i*(taillePointille+8)+taillePointille,LIMITEROUTEM);
       //On dessine les pointilles de la route, 8 et espacePointilles sont les espaces entre les pointillés, ils doivent être égaux
     }
-    
+
     espacePointilles-=2;
     if (espacePointilles<-taillePointille-4) {espacePointilles=2;};
 }
 void dObstacles()
 {
-  
+for (i=0; i<3; i++)
+{
+  switch (obs[i].nBitmap)
+  {
+    case 0: gb.display.drawBitmap(obs[i].x-=2,obs[i].y,cone);
+    break;
+    case 1: gb.display.drawBitmap(obs[i].x-=2,obs[i].y,clous);
+    break;
+    case 2: gb.display.drawBitmap(obs[i].x-=2,obs[i].y,animal);
+    break;
+    case 3: gb.display.drawBitmap(obs[i].x-=2,obs[i].y,homme);
+    break;
+
+  }
+  if (obs[i].x<=-8) 
+  {
+    obs[i].x = 84;
+    obs[i].y = random(2)*12+14;
+    obs[i].nBitmap = random(4);
+  }
+}
 }
